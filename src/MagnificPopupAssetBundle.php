7f430b5c30f61b93b13afea43ca59cc2d61b650e
effsoft <herilan@hotmail.com>
2019-03-18 21:52:26 +0800

    init

---
+++ src/MagnificPopupAssetBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace effsoft\eff\asset\magnific\popup;
+
+use effsoft\eff\EffAssetBundle;
+
+class MagnificPopupAssetBundle extends EffAssetBundle{
+
+    public $sourcePath = '@bower/magnific-popup/dist';
+
+    public function init(){
+        parent::init();
+
+    }
+
+    public $css = [
+        'magnific-popup.css',
+    ];
+    public $js = [
+        YII_ENV_DEV ? 'jquery.magnific-popup.js':'jquery.magnific-popup.min.js',
+    ];
+
+}
